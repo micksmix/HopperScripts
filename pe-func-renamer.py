258d9c9433440b6021205cb054934c79dd2e7c9a
MicksMix <mgrove@gmail.com>
2014-06-30 08:57:31 -0700

    Update pe-func-renamer.py

--- pe-func-renamer.py
+++ pe-func-renamer.py
@@ -6,7 +6,7 @@
 #    This script will generically rename a function within Hopper, if the Windows PE binary contains certain Windows API 
 #      names identified by Hopper as imports.       
 # Author:
-#    This is a "Hopper" port by Mick Grove (micksmix.wordpress.com) of Alexander Hanel's "func-renamer.py" script for IDA Pro.
+#    This is a "Hopper" port of Alexander Hanel's "func-renamer.py" script for IDA Pro.
 #      *All* credit for the work behind this script goes to Alexander Hanel: alexander<dot>hanel<at>gmail<dot>com
 #      Alexander's website is at: http://hooked-on-mnemonics.blogspot.com/
 #
